0ad52c4e7b8c7a2088ac3a235f9f65cc22f51c32
root <root@localhost.localdomain>
2016-02-12 01:42:41 -0500

    gcc compiles

--- sensors.c
+++ sensors.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
 
-const char * flowOutFile = "/sys/devices/12d10000.adc/iio:device0/in_voltage0_raw";
-const char * flowInFile = "/sys/devices/12d10000.adc/iio:device0/in_voltage1_raw";
+char * flowOutFile = 
+	"/sys/devices/12d10000.adc/iio:device0/in_voltage0_raw";
+char * flowInFile = 
+	"/sys/devices/12d10000.adc/iio:device0/in_voltage1_raw";
 
-struct timespec tim, timrem;
-tim.tv_sec = 0;
-tim.tv_nsec = 1000000L;
+struct timespec tim;
+struct timespec timrem;
 
-unsigned int lastFlowOut, lastFlowIn, thisFlowOut, thisFlowIn, 
+unsigned int lastFlowOut, lastFlowIn, thisFlowOut, thisFlowIn;
 signed int flowCountOut, flowCountIn;
 
 struct timeval lastTime, thisTime;
@@ -35,13 +38,15 @@ int analogRead(char * fName) {
 
 int main(int argc, char** argv)
 {
-
   lastFlowOut = analogRead(flowOutFile);
   lastFlowIn = analogRead(flowInFile);
   gettimeofday( &lastTime, NULL );
   int iterationCount = 0;
   
-  while(nanosleep(tim, timrem) == 0){
+  tim.tv_sec = 0;
+  tim.tv_nsec = 1000000L;
+ 
+  while(nanosleep(&tim, &timrem) == 0){
     // get flow out
     thisFlowOut = analogRead(flowOutFile);
     if( thisFlowOut != lastFlowOut ){
@@ -52,24 +57,30 @@ int main(int argc, char** argv)
     // get flow in
     thisFlowIn = analogRead(flowInFile);
     if( thisFlowIn != lastFlowIn ){
-      flowCountOut++
+      flowCountOut++;
     }
     lastFlowIn = thisFlowIn;
     
     // log flow rate if at limit
     if( iterationCount == 1000 ){
       gettimeofday( &thisTime, NULL );
-      float elapsed = (float)(((long long int)thisTime.tv_sec * 1000000 + thisTime.tv_usec) - ((long long int)lastTime.tv_sec * 1000000 + lastTime.tv_usec));
-      float outFrequency = flowCountOut / elapsed
-      float inFrequency = flowCountIn / elapsed
+      float elapsed = (float)(((long long int)thisTime.tv_sec * 1000000L + thisTime.tv_usec) - ((long long int)lastTime.tv_sec * 1000000L + lastTime.tv_usec))/(double)1000000;
+      float outFrequency = flowCountOut / elapsed;
+      float inFrequency = flowCountIn / elapsed;
       
       float outQ = outFrequency / 5.5;
       float inQ = inFrequency / 5.5;
       
+      printf("Elapsed: %f\n", elapsed);
       printf("Out Q: %f\n", outQ);
-      printf("In  Q: $f\n", inQ);
-      
+      printf("In  Q: %f\n", inQ);
+     
+      lastTime = thisTime; 
+      flowCountOut = 0;
+      flowCountIn = 0;
       iterationCount = 0;
+    } else {
+      iterationCount++;
     }
   }
   
