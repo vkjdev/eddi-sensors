bcf1fbdde6b1eca5b7eed9f09aa6d8855becbd37
Jesse Lee <mienaikoe@gmail.com>
2016-02-21 10:38:59 -0500

    fixing some bugs

--- persistence.c
+++ persistence.c
@@ -7,11 +7,6 @@
 #define DEBUG 1
 
 
-CURL *curleasy;
-CURLM *curlmulti;
-int multihandles;
-
-char * EDDI_ID;
 FILE * PERSIST_FILE;
 unsigned long DAY_SINCE_EPOCH;
 
@@ -25,18 +20,14 @@ void checkDataFile(){
       fprintf(stderr, "Could not close file!");
       exit(1);
     }
-    PERSIST_FILE = fopen(sprintf("data/%Lu",currentDay), "a");
+    char * fileName;
+    sprintf(fileName, "data/%Lu", currentDay);
+    PERSIST_FILE = fopen(fileName, "a");
     DAY_SINCE_EPOCH = currentDay;
   }
 }
 
 int persistenceInitialize(){
-  EDDI_ID = getenv("EDDI_ID");
-  if( EDDI_ID === NULL ){
-    fprintf(stderr, "EDDI_ID Environment Variable must be set");
-    exit(1);
-  }
-
   checkDataFile();
 }
 
@@ -46,7 +37,7 @@ void persistSenseSet(struct SenseSet *set){
   char * dataEntry;
   sprintf(dataEntry, "%Lu|%f|%f|%d|%d|%d", set.timestamp, set.qOut, set.qDump, set.ppmOut, set.ppmIn, set.ppmRec);
 
-  frwite(dataEntry, sizeOf(dataEntry[0]), sizeOf(dataEntry)/sizeOf(dataEntry[0]), PERSIST_FILE);
+  fwrite(dataEntry, sizeof(dataEntry[0]), sizeof(dataEntry)/sizeof(dataEntry[0]), PERSIST_FILE);
 }
 
 void persistenceCleanup(){
